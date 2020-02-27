ba5cfbc8c87db00c163d94ae05a05e2e3110e79d
vijayan.r <vijayan.r@hcl.com>
2020-02-27 16:14:34 +0530

    main class

--- src/main/java/com/mock/Shopping/App.java
+++ src/main/java/com/mock/Shopping/App.java
@@ -1,13 +1,12 @@
 package com.mock.Shopping;
 
-/**
- * Hello world!
- *
- */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+		public static void main(String[] args) {
+			SpringApplication.run(App.class, args);
+		}
 }
